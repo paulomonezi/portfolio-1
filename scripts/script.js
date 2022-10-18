4ad05d331294288383d7c10513bfe0de0574a945
Paulo Monezi <paulo.monezi@icloud.com>
2022-10-17 23:15:59 -0400

    :package: budget calculation

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,25 @@ document.querySelector(".hamburguer")
             .classList.toggle("show-menu")
     })
 
+document.querySelector("#pages").addEventListener("change", updatePrice)
+document.querySelector("#js").addEventListener("change", updatePrice)
+document.querySelector("#layout-yes").addEventListener("change", updatePrice)
+document.querySelector("#layout-no").addEventListener("change", updatePrice)
+document.querySelector("#deadline").addEventListener("change", () => {
+    const deadline = document.querySelector("#deadline").value
+    document.querySelector("label[for=deadline]").innerHTML = `Prazo: ${deadline} semanas`
+    updatePrice()
+})
+
+function updatePrice() {
+    const pages = document.querySelector("#pages").value
+    const hasJS = document.querySelector("#js").checked
+    const needLayout = document.querySelector("#layout-yes").checked
+    const deadline = document.querySelector("#deadline").value
+    let price = pages * 100;
+    if (hasJS) price *= 1.1
+    if (needLayout) price += 500
+    let deadlineBudgetTax = 1 - deadline * 0.1;
+    price *= 1 + deadlineBudgetTax
+    document.querySelector("#price").innerHTML = `R$ ${price.toFixed(2)}`
+}
